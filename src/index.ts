fb962d4718c8284f3b04dbc44cffcf6a60e0d7d3
tokisakikurumi <563693600@qq.com>
2022-01-04 21:00:44 +0800

    Feat::sparkles: :tada: 获取回响数据，中转服务器

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import axios from 'axios';
+import { createServer } from 'http';
+
+const app = express();
+const rootRouter = express.Router({ strict: true });
+
+app.get('/api/core', async (req, res) => {
+   try {
+    const commitRes = await axios.get('https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/commits');
+    const data = commitRes.data;
+    const lastCommit = data[0];
+    const json = await axios.get(`https://gitee.com/shanzhaikabi/elernal-tree/raw/${lastCommit.sha}/data/core.json`);
+    res.send(json.data);
+   } catch (error) {
+       res.send([]);
+   }
+});
+
+
+const sever = createServer(app);
+sever.listen('8080');
