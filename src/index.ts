a4642c8f2f777f8426852ec3ead589f6da781861
tokisakikurumi <563693600@qq.com>
2022-01-04 22:15:34 +0800

    Feat::sparkles: 失败返回已有数据

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
-import express from 'express';
-import axios from 'axios';
-import { createServer } from 'http';
-import fs from 'fs';
+import express from "express";
+import axios from "axios";
+import { createServer } from "http";
+import fs from "fs";
 
 const app = express();
 const rootRouter = express.Router({ strict: true });
-let sha = '';
+let sha = "";
 
-app.get('/api/core', async (req, res) => {
-   try {
-    const commitRes = await axios.get('https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/commits');
+app.get("/api/core", async (req, res) => {
+ 
+  try {
+    const commitRes = await axios.get(
+      "https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/commits",{
+          timeout: 10000
+      }
+    );
     const data = commitRes.data;
     const lastCommit = data[0];
     let json = [];
     if (sha != lastCommit.sha) {
-        sha = lastCommit.sha;
-        const  res = await axios.get(`https://gitee.com/shanzhaikabi/elernal-tree/raw/${lastCommit.sha}/data/core.json`);
-        json = res.data;
-        fs.writeFileSync('./core.json', JSON.stringify(json));
+      sha = lastCommit.sha;
+      const res = await axios.get(
+        `https://gitee.com/shanzhaikabi/elernal-tree/raw/${lastCommit.sha}/data/core.json`, {
+            timeout: 10000
+        }
+      );
+      json = res.data;
+      fs.writeFileSync("./core.json", JSON.stringify(json));
     } else {
-        json = JSON.parse(fs.readFileSync('./core.json').toString());
+      json = JSON.parse(fs.readFileSync("./core.json").toString());
     }
-   
+
     res.send(json);
-   } catch (error) {
-       res.send([]);
-   }
+  } catch (error) {
+    let json = [];
+    json = JSON.parse(fs.readFileSync("./core.json").toString());
+    res.send(json);
+  }
 });
 
 const sever = createServer(app);
-sever.listen('5006');
+sever.listen("5006");
