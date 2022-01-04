77cb96dbe90d2cdda591f32f141badc9bc2d49ff
tokisakikurumi <563693600@qq.com>
2022-01-04 21:49:44 +0800

    Feat::sparkles: sha 缓存

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import express from 'express';
 import axios from 'axios';
 import { createServer } from 'http';
+import fs from 'fs';
 
 const app = express();
 const rootRouter = express.Router({ strict: true });
+let sha = '';
 
 app.get('/api/core', async (req, res) => {
    try {
     const commitRes = await axios.get('https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/commits');
     const data = commitRes.data;
     const lastCommit = data[0];
-    const json = await axios.get(`https://gitee.com/shanzhaikabi/elernal-tree/raw/${lastCommit.sha}/data/core.json`);
-    res.send(json.data);
+    let json = [];
+    if (sha != lastCommit.sha) {
+        sha = lastCommit.sha;
+        const  res = await axios.get(`https://gitee.com/shanzhaikabi/elernal-tree/raw/${lastCommit.sha}/data/core.json`);
+        json = res.data;
+        fs.writeFileSync('./core.json', JSON.stringify(json));
+    } else {
+        json = JSON.parse(fs.readFileSync('./core.json').toString());
+    }
+   
+    res.send(json);
    } catch (error) {
        res.send([]);
    }
