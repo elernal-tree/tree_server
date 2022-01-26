7e6ddc32bd44353f8971d35fec182a96fd496ed5
zafkielkurumi <563693600@qq.com>
2022-01-26 12:50:08 +0800

    Feat::sparkles: 无用回响过滤

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,43 @@ const app = express();
 const rootRouter = express.Router({ strict: true });
 let sha = "";
 
+function getCore() {
+  const coreList: any[] = JSON.parse(fs.readFileSync("./core.json").toString());
+  const outCoreList = JSON.parse(fs.readFileSync("./out_core.json").toString());
+  return coreList.filter(core => !outCoreList.includes(core.id));
+}
+
+function setCore(json) {
+  fs.writeFileSync("./core.json", JSON.stringify(json));
+}
+
 app.get("/api/core", async (req, res) => {
- 
   try {
     const commitRes = await axios.get(
-      "https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/branches/release",{
-          timeout: 10000
+      "https://gitee.com/api/v5/repos/shanzhaikabi/elernal-tree/branches/master",
+      {
+        timeout: 10000,
       }
     );
     const lastSha = commitRes.data.commit.sha;
-    console.log(sha, 'sha', lastSha);
+    console.log(sha, "sha", lastSha);
     let json = [];
     if (sha != lastSha) {
-      console.log(lastSha);
       sha = lastSha;
       const res = await axios.get(
-        `https://gitee.com/shanzhaikabi/elernal-tree/raw/${sha}/data/core.json`, {
-            timeout: 10000
+        `https://gitee.com/shanzhaikabi/elernal-tree/raw/${sha}/data/core.json`,
+        {
+          timeout: 10000,
         }
       );
       json = res.data;
-      fs.writeFileSync("./core.json", JSON.stringify(json));
-    } else {
-      json = JSON.parse(fs.readFileSync("./core.json").toString());
+      setCore(json);
     }
-
+    json = getCore();
     res.send(json);
   } catch (error) {
     let json = [];
-    json = JSON.parse(fs.readFileSync("./core.json").toString());
+    json = getCore();
     res.send(json);
   }
 });
